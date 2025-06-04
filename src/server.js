f63ef7b240fd392ad873762267f5ae99704970a5
Jerome Doran <disneyfan9184@gmail.com>
2025-06-04 16:13:39 -0400

    added cron job routine

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rate-limiter.js';
 import transactionRoute from './routes/transactions.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
+
+// Start the cron job
+if (process.env.NODE_ENV === 'production') job.start();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +18,10 @@ app.use(express.json()); // built-in middleware for parsing JSON bodies
 app.use(rateLimiter); // custom middleware for rate limiting
 
 const PORT = process.env.PORT || 5001;
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 app.use('/api/transactions', transactionRoute);
 
